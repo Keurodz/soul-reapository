f40ea35a8a56439168270ac2f12ecaa0fa6218c0
Keurodz <77088132+Keurodz@users.noreply.github.com>
2024-02-17 01:22:13 -0500

    Greybox Partially Complete

---
+++ Reapossesion Inc/Assets/Scripts/GraveLerp.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraveLerp : MonoBehaviour
+{
+    public Color startColor;
+    public Color endColor;
+    public Light SpotLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpotLight.color = Color.Lerp(startColor, endColor, Mathf.PingPong(Time.time, 1));
+    }
+}
